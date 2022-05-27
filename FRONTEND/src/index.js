4c2a4487d97f75fc27366d83e1a6c149583c1426
Leonardo Westley <leonardowestley@Leonardos-MacBook-Air.local>
2022-05-27 15:55:09 -0400

    started new app

--- FRONTEND/src/index.js
+++ FRONTEND/src/index.js
@@ -4,6 +4,15 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+const firebaseConfig = {
+  apiKey: "AIzaSyB7hdUzqbC0jDoRP5VPVm4DA5osZUHF7Ck",
+  authDomain: "practice-stack-app.firebaseapp.com",
+  projectId: "practice-stack-app",
+  storageBucket: "practice-stack-app.appspot.com",
+  messagingSenderId: "792013258002",
+  appId: "1:792013258002:web:93ef33eb92c4438f8187cb"
+};
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
